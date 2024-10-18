4f837e3d4d171a47f81cdf6f94a2d3271898032e
stodolakmichal <stodolak.michal@gmail.com>
2024-10-18 22:19:37 +0200

    Test_add_entry added

--- tests/tests_main/test_CSV_class.py
+++ tests/tests_main/test_CSV_class.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from main import CSV
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
 
 class TestCSV:
@@ -17,3 +17,18 @@ class TestCSV:
                 df = pd.DataFrame(columns=CSV.COLUMNS)
                 mock_to_csv.assert_called_once_with(CSV.CSV_FILE, index=False)
                 assert list(df.columns) == CSV.COLUMNS
+
+    def test_add_entry(self):
+        entry = {
+            "date": "01-01-2020",
+            "amount": "350",
+            "category": "Test_category",
+            "description": "Test_description"
+        }
+        with patch('builtins.open', mock_open()) as mock_file:
+            with patch('csv.DictWriter') as mock_DictWriter:
+                mock_writer_instance = mock_DictWriter.return_value
+                CSV.add_entry(**entry)
+                mock_file.assert_called_once_with(CSV.CSV_FILE, "a", newline="")
+                mock_DictWriter.assert_called_once_with(mock_file(), fieldnames=CSV.COLUMNS)
+                mock_writer_instance.writerow.assert_called_once_with(entry)
